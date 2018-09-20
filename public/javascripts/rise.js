af39b0da65763d1a9cf36c3a8f19c9a2b7e5afe1
Aldair47x <aldair47x@gmail.com>
2018-09-20 11:10:19 -0500

    rise test 1.2

--- public/javascripts/rise.js
+++ public/javascripts/rise.js
@@ -1,12 +1,5 @@
-$('.carousel').carousel({
-    interval: 2000
-  })
-
-const myCarousel = document.querySelector('#myCarousel');
-myCarousel.addEventListener("mouseover", function() {
-    fetch("zapato.json")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    })
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 2000
+      })
 });
